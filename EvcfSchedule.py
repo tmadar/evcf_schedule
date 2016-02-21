aaff548bfd1409b8e67ab84a6e523fbf5c4ccc30
travis madar <travismadar@traviss-MacBook-Pro.local>
2016-02-20 23:58:44 -0700

    turn day schedule and workouts list into service

--- EvcfSchedule.py
+++ EvcfSchedule.py
@@ -1,13 +1,40 @@
 import urllib
 from bs4 import BeautifulSoup
+from flask import Flask
+app = Flask(__name__)
 
-url = 'https://eastvalleycrossfit.sites.zenplanner.com/calendar.cfm'
-page = urllib.urlopen(url)
-soup = BeautifulSoup(page, "html.parser")
+@app.route('/schedule')
+def getTodaysSchedule():
+    url = 'https://eastvalleycrossfit.sites.zenplanner.com/calendar.cfm'
+    page = urllib.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
+    todayList = soup.find("td", { "class" : "today" }).text
+    todayList = todayList.replace(" ", "\n")
+    return todayList, 200, {'Content-Type': 'text/css; charset=utf-8'}
 
-todayList = soup.find("td", { "class" : "today" }).text
+@app.route('/workouts')
+def getWorkouts():
+    list = ""
+    url = 'http://eastvalleycrossfit.com/'
+    page = urllib.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
+    fullList = soup.find_all("div", { "class" : "hentry" })
+    for entry in fullList:
+        list += entry.text
+        # print(entry.text)
+    return list, 200, {'Content-Type': 'text/css; charset=utf-8'}
 
-print todayList
+if __name__ == '__main__':
+    app.run()
+
+# list = ""
+# url = 'http://eastvalleycrossfit.com/'
+# page = urllib.urlopen(url)
+# soup = BeautifulSoup(page, "html.parser")
+# fullList = soup.find_all("div", { "class" : "hentry" })
+# for entry in fullList:
+#     list += entry.text
+# print(list)
 
 
 
